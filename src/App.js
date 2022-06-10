ddcbb12dca8bf7f0b39250710b599eb07261feb7
Jashanpreet Kaur <jashanpreet110703@gmail.com>
2022-06-10 12:28:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+
+// import './App.css';
+// import Navbar from './components/Navbar';
+// import TextForm from './components/TextForm';
+// import About from './components/About';
+// import React, { useState } from 'react';
+// import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
+// function App() {
+//   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+//   const [alert, setAlert] = useState(null);
+//   const showAlert = (message, type) => {
+//     setAlert({
+//       msg: message,
+//       type: type
+//     })
+//     setTimeout(() => {
+//       setAlert(null);
+//     }, 1500);
+//   }
+
+//   const removeBodyClasses = () => {
+//     document.body.classList.remove('bg-light')
+//     document.body.classList.remove('bg-dark')
+//     document.body.classList.remove('bg-warning')
+//     document.body.classList.remove('bg-danger')
+//     document.body.classList.remove('bg-success')
+//   }
+
+//   const toggleMode = (cls) => {
+//     removeBodyClasses();
+//     //console.log(cls)
+//     document.body.classList.add('bg-' + cls)
+//     const toggleMode = () => {
+//       if (mode === 'light') {
+//         setMode('dark');
+//         document.body.style.backgroundColor = '#042743';
+//         showAlert("Dark mode has been enabled", "success");
+//         showAlert("Dark mode has been enabled", "success");
+//       }
+//       else {
+//         setMode('light');
+//         document.body.style.backgroundColor = 'white';
+//         showAlert("Light mode has been enabled", "success");
+//         // document.title = 'TextUtils - Light Mode';
+//       }
+//     }
+//   }
+//     return (
+//       <>
+//         {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+//         {/* <Navbar/> */}
+//         <Router>
+//           <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+//           <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()}/>
+//           <Alert alert={alert} />
+//           <div className="container my-3">
+//             <Routes>
+//               {/*/users --> Component 1
+//         /users/home --> Component 2 */}
+//               <Route exact path="/about">
+//                 <About mode={mode} />
+//               </Route>
+//               <Route exact path="/">
+//                 <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />
+//               </Route>
+//             </Routes> 
+//           </div>
+//         </Router>
+//       </>
+//     );
+// }
+// export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value) 
+    }
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+    const [text, setText] = useState(''); 
+    return (
+        <>
+        <div className="container"> 
+            <h1 className='mb-4'>TryText</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="11" cols="70"></textarea>
+            </div>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
+        </div>
+        </>
+    )
+}
